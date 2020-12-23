aa7e5d70f8b531f121ed2e8e18ceaae7f5162fc6
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-22 22:10:49 -0800

    Updated reducer with new and upcoming games

--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,4 +1,4 @@
-import { bindActionCreators } from "redux";
+//import { bindActionCreators } from "redux";
 
 const initialState = {
   popular: [],
@@ -9,7 +9,12 @@ const initialState = {
 const gamesReducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_GAMES":
-      return { ...state, popular: action.payload.popular };
+      return {
+        ...state,
+        popular: action.payload.popular,
+        newGames: action.payload.newGames,
+        upcoming: action.payload.upcoming,
+      };
     default:
       return { ...state };
   }
