8d1f98aa69b6bce58b97c6e3e439c16957ad2ae1
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-24 14:09:44 -0800

    Added new and popular games to Game Action

--- src/actions/gameAction.js
+++ src/actions/gameAction.js
@@ -1,14 +1,18 @@
 import axios from "axios";
-import { popular_gamesURL } from "../api";
+import { popular_gamesURL, upcoming_gamesURL, new_gamesURL } from "../api";
 
 //Action Creator
 export const loadGames = () => async (dispatch) => {
   //Fetch data
   const popularData = await axios.get(popular_gamesURL());
+  const upcomingData = await axios.get(upcoming_gamesURL());
+  const newData = await axios.get(new_gamesURL());
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newData.data.results,
     },
   });
 };
