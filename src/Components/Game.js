e77fe64fa9c5f4b4595abbdad4c14b84849f4d43
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-23 22:36:45 -0800

    Rfactored Game Component

---
+++ src/Components/Game.js
@@ -0,0 +1,14 @@
+import React from "react";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+const Game = () => {
+  return (
+    <div>
+      <h3>Game Name</h3>
+      <p>Released Data</p>
+    </div>
+  );
+};
+
+export default Game;
