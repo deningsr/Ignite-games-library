68c025d64e1a747e02583d4c23cff1916ab3084f
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-24 14:06:24 -0800

    Creted Redux action for game details and screenshots

---
+++ src/actions/detailAction.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { gameDetailsURL, gameScrenshotURL } from "../api";
+
+export const loadDetail = (id) => async (dispatch) => {
+  const detailsData = await axios.get(gameDetailsURL(id));
+  const screenShotData = await axios.get(gameScrenshotURL(id));
+  dispatch({
+    type: "GET_DETAIL",
+    payload: {
+      game: detailsData.data,
+      screen: screenShotData.data,
+    },
+  });
+};
