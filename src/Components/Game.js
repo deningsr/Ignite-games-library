1a4adc62b7fb259e5b46266c6c74ab31c0d9568c
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-24 13:06:29 -0800

    Added Game stylings and rendered games

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -2,13 +2,26 @@ import React from "react";
 //Styling and Animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
-const Game = () => {
+const Game = ({ name, released, image }) => {
   return (
-    <div>
-      <h3>Game Name</h3>
-      <p>Released Data</p>
-    </div>
+    <StyledGame>
+      <h3>{name}</h3>
+      <p>{released}</p>
+      <img src={image} alt={name} />
+    </StyledGame>
   );
 };
 
 export default Game;
+
+const StyledGame = styled(motion.div)`
+  min-height: 30vh;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  border-radius: 1rem;
+  img {
+    width: 100%;
+    heightL 40vh;
+    object-fit: cover;
+  }
+`;
