30a098faf8bdee494e16d83b7757557e2439124c
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-25 16:20:53 -0800

    Imported and added React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Home from "./pages/Home";
 import React from "react";
 import GlobalStyles from "./Components/GlobalStyles";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <Home />
+      <Route path={["/game/:id", "/"]}>
+        <Home />
+      </Route>
     </div>
   );
 }
