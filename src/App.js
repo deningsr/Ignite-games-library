201cef9e900f524705597f3767655926c2d9cd16
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-26 14:53:58 -0800

    rendered Nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./pages/Home";
 import React from "react";
 import GlobalStyles from "./Components/GlobalStyles";
+import Nav from "./Components/Nav";
 import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
+      <Nav />
       <Route path={["/game/:id", "/"]}>
         <Home />
       </Route>
