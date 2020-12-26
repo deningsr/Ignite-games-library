82e60e544e8c4deaa8bd3b1db15721102ae5a784
Denin Grcic <deningrcic@MacBook-Pro.local>
2020-12-26 12:39:21 -0800

    Implemented resize image helper function

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -4,16 +4,22 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useDispatch } from "react-redux";
 import { loadDetail } from "../actions/detailAction";
+import { Link } from "react-router-dom";
+import { smallImage } from "../util";
+
 const Game = ({ name, released, image, id }) => {
   const dispatch = useDispatch();
   const loadDetailHandler = () => {
+    document.body.style.overflow = "hidden";
     dispatch(loadDetail(id));
   };
   return (
     <StyledGame onClick={loadDetailHandler}>
-      <h3>{name}</h3>
-      <p>{released}</p>
-      <img src={image} alt={name} />
+      <Link to={`/game/${id}`}>
+        <h3>{name}</h3>
+        <p>{released}</p>
+        <img src={smallImage(image, 640)} alt={name} />
+      </Link>
     </StyledGame>
   );
 };
@@ -25,6 +31,8 @@ const StyledGame = styled(motion.div)`
   box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.2);
   text-align: center;
   border-radius: 1rem;
+  cursor: pointer;
+  overflow: hidden;
   img {
     width: 100%;
     heightL 40vh;
